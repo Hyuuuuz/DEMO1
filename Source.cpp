a8dc0db5227cf73059c7594118e3155bb870181e
unknown <dh52103404@student.stu.edu.vn>
2024-11-02 11:23:44 +0700

    source Tong

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b) {
+	return a + b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout << "Nhap a";
+	cin >> a;
+	cout << "Nhap b";
+	cin >> b;
+	cout << "Tong la: " << tong(a, b) << endln;
 
 	system("pause");
 	return 0;

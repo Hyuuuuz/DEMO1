7d988a98249d17303a9cbb42360339b1c4724db5
Huyneee <dh52103404@student.stu.edu.vn>
2024-11-09 11:24:39 +0700

    add SNT

--- Source.cpp
+++ Source.cpp
@@ -8,19 +8,45 @@ int hieu(int a, int b);
 int tich(int a, int b) {
 	return a * b;
 }
+int soNT(int a) {
+	int dem = 0;
+	for (int i = 1; i <= sqrt(n); i++) {
+		if (n % i == 0) {
+			++dem;
+			if (i != n / i) {
+				++dem;
+			}
+		}
+	}
+	if (dem == 2) {
+		return 1; // true
+	}
+	else {
+		return 0; // false;
+	}
+}
 float thuong(int a, int b);
 int main()
 {
+
+
+
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+
+
+
 	cout << "Nhap a";
 	cin >> a;
-	cout << "Nhap b";
-	cin >> b;
-	cout << "Tong la: " << tong(a, b);
+	cout<<soNT(a)
 
-	cout << "Nhap x,y";
-	cin >> y >> x;
-	cout << "Tich la: " << tich(x, y);
+
+	//cout << "Nhap b";
+	//cin >> b;
+	//cout << "Tong la: " << tong(a, b);
+
+	//cout << "Nhap x,y";
+	//cin >> y >> x;
+	//cout << "Tich la: " << tich(x, y);
 
 	system("pause");
 	return 0;
